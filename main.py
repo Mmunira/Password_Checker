12642651571ca8cafd125a5f6d631b7366efd568
Munira Mohamed <muniramohamed@Muniras-MBP.broadband>
2021-06-15 12:05:55 +0100

    input user

--- main.py
+++ main.py
@@ -2,21 +2,29 @@
 class PasswordChecker:
     def take_input(self):
         #Take full user name, year of birth. as user what option (generate, check, or exit).
+        user_choice = None
         while True:
             options_choices = input('please enter one of the following options: check password(1), generate password(2) or exit()3 ')
 
-            if options_choices == ('3'):
-                password = input('exit password')
+            if options_choices in ('1', '2', '3'):
+                user_choice = options_choices
                 break
-            first_name = input('Enter your first name:')
-            last_name = input('Enter your last name:')
-            birth_year = input('Enter your year of birth:')
-            # options_choices = input('please enter one of the following options: check password, generate password or exit ')
-            if options_choices == ('1'):
-                password = input('Please input password to check: ')
-            elif options_choices == ('2'):
-                continue
             else:
-                print ('Please choose a valid option')
+                print('Please choose a valid option')
+
+        if user_choice == '3':
+            exit(0)
+
+        first_name = input('Enter your first name:')
+        last_name = input('Enter your last name:')
+        birth_year = input('Enter your year of birth:')
+
+        if user_choice == '1':
+            password = input('Please input password to check: ')
+        elif user_choice == '2':
+            pass
+
+
 password_checker = PasswordChecker()
 password_checker.take_input()
+
